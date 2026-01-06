b770d1e53615dc84961a3d8d47632f26713f79d2
webuser-1109 <m88771109@gmail.com>
2026-01-06 18:00:23 +0500

    added course card component

---
+++ src/components/blocks/course-card/index.tsx
@@ -0,0 +1,65 @@
+import { Badge } from "@/components/ui/badge";
+import type { ICourse } from "@/types";
+import { Calendar } from "lucide-react";
+
+const CourseCard = ({
+  course,
+  isOwnCourse = false,
+}: {
+  course: ICourse;
+  isOwnCourse?: boolean;
+}) => {
+  return (
+    // w-[360px] h-auto
+    <div className="flex flex-col gap-2 justify-between bg-card rounded-md border cursor-pointer hover:bg-card-foreground/10 duration-200 hover:ring-1 hover:ring-primary">
+      <div className="w-full h-60 relative">
+        <img
+          src={course?.img ?? "/logo.svg"}
+          className="w-full h-full object-cover aspect-square border-b"
+        />
+        {!isOwnCourse && (
+          <img
+            src={course.owner.avatar ?? "/logo.svg"}
+            className="absolute bottom-0 right-0 object-cover rounded-full"
+          />
+        )}
+      </div>
+      <div className="p-4">
+        <div className="w-full flex flex-row items-center justify-between mb-4">
+          <div>
+            <Badge
+              variant={
+                course.type === "public"
+                  ? "default"
+                  : course.type === "private"
+                    ? "outline"
+                    : course.type === "archived"
+                      ? "secondary"
+                      : "destructive"
+              }
+            >
+              {course.type}
+            </Badge>
+          </div>
+
+          <div className="flex justify-end items-center gap-1.5">
+            <Calendar className="text-muted-foreground" size={16} />
+            <p className="text-sm text-muted-foreground">
+              {new Date().toLocaleDateString("ru-RU", {
+                day: "2-digit",
+                month: "2-digit",
+                year: "numeric",
+              })}
+            </p>
+          </div>
+        </div>
+        <h3 className="text-2xl font-bold">{course?.name}</h3>
+        <p className="text-muted-foreground text-sm line-clamp-3">
+          {course?.desc}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default CourseCard;
--- src/pages/my-courses/courses.tsx
+++ src/pages/my-courses/courses.tsx
@@ -1,3 +1,4 @@
+import CourseCard from "@/components/blocks/course-card";
 import { Button } from "@/components/ui/button";
 import Centered from "@/components/ui/centered";
 import { Spinner } from "@/components/ui/spinner";
@@ -5,7 +6,7 @@ import { axiosClient } from "@/lib/axios";
 import { useAuthStore } from "@/store/auth";
 import type { ICourse } from "@/types";
 import { useQuery } from "@tanstack/react-query";
-import { Calendar, Plus } from "lucide-react";
+import { Plus } from "lucide-react";
 import { Link } from "react-router";
 
 const MyCourses = () => {
@@ -26,6 +27,8 @@ const MyCourses = () => {
       </Centered>
     );
 
+  console.log(data[0]);
+
   return (
     <div className="p-5">
       <div className="flex justify-between">
@@ -39,27 +42,10 @@ const MyCourses = () => {
           </Button>
         </Link>
       </div>
-      <div className="grid grid-cols-3 gap-4 mt-5">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-5">
         {data?.map((course) => (
-          <Link
-            to={`/course/${course?.id}`}
-            key={course?.id}
-            className="flex flex-col gap-2 justify-between bg-card w-full h-45 p-4 rounded-md border cursor-pointer hover:bg-card-foreground/10"
-          >
-            <h3 className="text-2xl font-bold">{course?.name}</h3>
-            <p className="text-muted-foreground text-sm line-clamp-3">
-              {course?.desc}
-            </p>
-            <div className="flex justify-end items-center gap-1.5">
-              <Calendar className="text-muted-foreground" size={16} />
-              <p className="text-sm text-muted-foreground">
-                {new Date().toLocaleDateString("ru-RU", {
-                  day: "2-digit",
-                  month: "2-digit",
-                  year: "numeric",
-                })}
-              </p>
-            </div>
+          <Link to={`/course/${course?.id}`} key={course?.id}>
+            <CourseCard course={course} isOwnCourse={true} />
           </Link>
         ))}
       </div>
